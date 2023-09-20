2c202343b5949a735eba17f94d19b30057fc9d3f
crunchydosa123 <prathamgadkari@gmail.com>
2023-09-20 23:56:31 +0530

    react setup
    for assignment 2:
    - installed nodejs
    - started a react app
    - made a navbar in react

---
+++ assignment 2/memory-card-flip-game/src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Navbar from './components/navbar';
+import './App.css' 
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment 2/memory-card-flip-game/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+// Navbar.js
+import React from 'react';
+import './navbar.css'; // Import CSS for styling
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>Memory Card Game using React</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
