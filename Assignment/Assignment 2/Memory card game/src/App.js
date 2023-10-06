b2930ecfb529514cc4a588c0865b1b046b5e6399
Tanishka-Jagtap <131904030+Tanishka-Jagtap@users.noreply.github.com>
2023-10-06 11:38:32 +0530

    Update App.js

--- Assignment/Assignment 2/Memory card game/src/App.js
+++ Assignment/Assignment 2/Memory card game/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -11,105 +11,129 @@ const cardImages = [
   {"src": "/img/Alakazam.png", matched: false}
 ];
 
-function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-  const [matchedPairs, setMatchedPairs] = useState(0);
-  const [showWinModal, setShowWinModal] = useState(false);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      turns: 0,
+      choiceOne: null,
+      choiceTwo: null,
+      disabled: false,
+      matchedPairs: 0,
+      showWinModal: false
+    };
+  }
 
-  const shuffleCards = () => {
+  shuffleCards = () => {
     const shuffleCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random() }));
+      .map((card, index) => ({ ...card, id: index + 1 }));
 
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setCards(shuffleCards);
-    setTurns(0);
-    setMatchedPairs(0);
-    setShowWinModal(false);
+    this.setState({
+      choiceOne: null,
+      choiceTwo: null,
+      cards: shuffleCards,
+      turns: 0,
+      matchedPairs: 0,
+      showWinModal: false
+    });
   };
 
-  const handleChoice = (card) => {
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  handleChoice = (card) => {
+    const { choiceOne, choiceTwo, turns } = this.state;
+
+    if (!choiceOne) {
+      this.setState({ choiceOne: card });
+    } else {
+      this.setState({ choiceTwo: card, turns: turns + 1, disabled: true });
+    }
   };
 
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns((prevTurns) => prevTurns + 1);
-    setDisabled(false);
+  resetTurn = () => {
+    this.setState({
+      choiceOne: null,
+      choiceTwo: null,
+      disabled: false
+    });
   };
 
-  useEffect(() => {
+  componentDidUpdate(prevProps, prevState) {
+    const { choiceOne, choiceTwo, cards, matchedPairs } = this.state;
+
     if (choiceOne && choiceTwo) {
-      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
-        setCards((prevCards) => {
-          const updatedCards = prevCards.map((card) =>
-            card.src === choiceOne.src ? { ...card, matched: true } : card
-          );
+        const updatedCards = cards.map((card) =>
+          card.src === choiceOne.src ? { ...card, matched: true } : card
+        );
 
-          const newMatchedPairs = updatedCards.filter((card) => card.matched).length;
-          setMatchedPairs(newMatchedPairs);
+        const newMatchedPairs = updatedCards.filter((card) => card.matched).length;
 
-          return updatedCards;
+        this.setState({
+          cards: updatedCards,
+          matchedPairs: newMatchedPairs,
+          disabled: false,
+          choiceOne: null,
+          choiceTwo: null
         });
-
-        resetTurn();
       } else {
-        setTimeout(() => resetTurn(), 1000);
+        setTimeout(() => {
+          this.setState({
+            disabled: false,
+            choiceOne: null,
+            choiceTwo: null
+          });
+        }, 1000);
       }
     }
-  }, [choiceOne, choiceTwo]);
 
-  useEffect(() => {
     if (matchedPairs === cardImages.length) {
-      setShowWinModal(true);
+      this.setState({ showWinModal: true });
     }
-  }, [matchedPairs]);
+  }
 
-  const handleModalClose = () => {
-    setShowWinModal(false);
-    shuffleCards();
+  handleModalClose = () => {
+    this.setState({ showWinModal: false });
+    this.shuffleCards();
   };
 
-  useEffect(() => {
-    shuffleCards();
-  }, []);
+  componentDidMount() {
+    this.shuffleCards();
+  }
 
-  return (
-    <div className="App">
-      <h1>Memory Card Flip Game</h1>
-      <button onClick={shuffleCards}>Restart Game</button>
+  render() {
+    const { cards, turns, disabled, choiceOne, choiceTwo, showWinModal } = this.state;
 
-      <div className="card-grid">
-        {cards.map((card) => (
-          <SingleCard
-            key={card.id}
-            card={card}
-            handleChoice={handleChoice}
-            flipped={card === choiceOne || card === choiceTwo || card.matched}
-            disabled={disabled}
-          />
-        ))}
-      </div>
-      <p>Turns: {turns}</p>
+    return (
+      <div className="App">
+        <h1>Memory Card Flip Game</h1>
+        <button onClick={this.shuffleCards}>Restart Game</button>
 
-      {showWinModal && (
-        <div className="win-modal">
-          <div className="win-modal-content">
-            <h2>Congratulations!</h2>
-            <p>You won the game in {turns} turns.</p>
-            <button onClick={handleModalClose}>Play Again</button>
-          </div>
+        <div className="card-grid">
+          {cards.map((card) => (
+            <SingleCard
+              key={card.id}
+              card={card}
+              handleChoice={this.handleChoice}
+              flipped={card === choiceOne || card === choiceTwo || card.matched}
+              disabled={disabled}
+            />
+          ))}
         </div>
-      )}
-    </div>
-  );
+        <p>Turns: {turns}</p>
+
+        {showWinModal && (
+          <div className="win-modal">
+            <div className="win-modal-content">
+              <h2>Congratulations!</h2>
+              <p>You won the game in {turns} turns.</p>
+              <button onClick={this.handleModalClose}>Play Again</button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
