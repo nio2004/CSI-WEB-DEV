773ec0e39929e621abc58a32ff4496d1c2e86baf
crunchydosa123 <prathamgadkari@gmail.com>
2023-10-13 13:33:23 +0530

    dark mode done

--- assignment 4/stock-market-app/src/App.js
+++ assignment 4/stock-market-app/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import './index.css'
 import Dashboard from './components/Dashboard';
+import React, { useState } from 'react';
+import ThemeContext from './context/ThemeContext';
+import StockContext from './context/StockContext';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [stockSymbol, setStockSymbol] = useState("FB");
   return (
-    <Dashboard />
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      <StockContext.Provider value={{stockSymbol, setStockSymbol}}>
+      <Dashboard />
+      </StockContext.Provider>
+    </ThemeContext.Provider>
   );
 }
 
--- assignment 4/stock-market-app/src/components/Chart.js
+++ assignment 4/stock-market-app/src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { mockHistoricalData } from '../constants/mock'
 import { convertDateToUnixTimestamp, convertUnixTimestampToDate } from '../helpers/date-helper';
 import Card from './Card';
@@ -10,12 +10,14 @@ import {
     XAxis,
     YAxis
 } from "recharts";
+import ThemeContext from '../context/ThemeContext';
 
 const Chart = () => {
-    const [data, setData] = useState(mockHistoricalData);
-    const [filter, setFilter] = useState("1W");
+  const { darkMode } = useContext(ThemeContext);
+  const [data, setData] = useState(mockHistoricalData);
+  const [filter, setFilter] = useState("1W");
 
-const formatData = (data) => {
+  const formatData = (data) => {
     return data.c.map((item, index) => {
       return {
         value: item.toFixed(2),
@@ -27,15 +29,40 @@ const formatData = (data) => {
   return (
     <Card>
         <ResponsiveContainer>
-            <AreaChart data={formatData(data)}>
-                <Area type='monotone' dataKey='value' stroke='#312e81' fillOpacity={1} strokeWidth={0.5} />
-                <Tooltip/>
-                <XAxis dataKey={"date"} />
-                <YAxis domain={["dataMin", "dataMax"]} />
-            </AreaChart>
+        <AreaChart data={formatData(data)}>
+          <defs>
+            <linearGradient id="chartColor" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="5%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="95%"
+                stopColor={darkMode ? "#312e81" : "rgb(199 210 254)"}
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <Tooltip
+            contentStyle={darkMode ? { backgroundColor: "#111827" } : null}
+            itemStyle={darkMode ? { color: "#818cf8" } : null}
+          />
+          <Area
+            type="monotone"
+            dataKey="value"
+            stroke="#312e81"
+            fill="url(#chartColor)"
+            fillOpacity={1}
+            strokeWidth={0.5}
+          />
+          <XAxis dataKey="date" />
+          <YAxis domain={["dataMin", "dataMax"]} />
+        </AreaChart>
         </ResponsiveContainer>
     </Card>
   )
 }
 
 export default Chart
+
