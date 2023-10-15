83c27306f355b7f1495bcc809fb6067d1bd380d1
crunchydosa123 <prathamgadkari@gmail.com>
2023-10-15 13:55:25 +0530

    with search bar

--- assignment 4/stock-market-app/src/Pages/Home.js
+++ assignment 4/stock-market-app/src/Pages/Home.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dashboard from '../components/Dashboard';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/Hero-Section';
 import Search from '../components/Search';
+import ThemeContext from '../context/ThemeContext';
 
 function Home() {
   return (
     <div>
+    <ThemeContext.Provider value={{ darkMode: false }}>
       <Navbar />
       <HeroSection />
+      <Search />
+      </ThemeContext.Provider>
     </div>
   );
 }
